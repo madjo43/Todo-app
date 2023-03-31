7534579193df16545a14a9fa164b5c4eb3216e82
Mladen Ivankovic <116635970+madjo43@users.noreply.github.com>
2023-03-31 19:39:24 +0200

    Added new list

--- src/App.jsx
+++ src/App.jsx
@@ -16,6 +16,7 @@ const defaultItems = [
 
 function App() {
   const [items, setItems] = useState(defaultItems);
+  const [formState, setFormState] = useState({text: ""});
 
   const itemComponents = items.map(item =>{
     const handleChange = () => {
@@ -28,16 +29,44 @@ function App() {
       }));
     };
 
+    const handleClick = () => {
+      setItems(items.filter(newItem =>{
+        return newItem.id !== item.id;
+      }));
+    };
+
     return (
       <div key ={item.id}>
         <input type="checkbox" checked={item.done} onChange={handleChange} />{item.text}
+        <button onClick={handleClick}>X</button>
       </div>
     );
   });
+ const handleChange = (event) =>{
+   setFormState({...formState,
+  [event.target.name]: event.target.value,});
+ }
 
+ const handleSubmit = (event) =>{
+   event.preventDefault();
+   setItems([...items,
+  {
+    id: Date.now(),
+    text: formState.text,
+    done:false,
+  },
+]);
+    setFormState({...formState, text:''});  
+ }
+
+  
   return (
     <div> 
       <h1>Todo-app</h1>
+      <form onSubmit={handleSubmit}>
+        <input type="text" name="text" value={formState.text} onChange={handleChange} />
+        <button type="submit" >Add item</button>
+      </form>
       {itemComponents}
     </div>
   )
